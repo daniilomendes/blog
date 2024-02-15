ff9feda8840eb9eef94cb0733d8e8b274b851f2f
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-15 20:58:04 -0300

    feat: add Header components na aplicação

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom"
-import Home from "./pages/Home"
-import About from './pages/About'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import Dashboard from './pages/Dashboard'
-import Projects from './pages/Projects'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -18,5 +20,5 @@ export default function App() {
         <Route path="/projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
