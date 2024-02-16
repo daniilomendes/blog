cc2e0b3ca6dbb119533bff1842ec20f728719d6a
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-15 21:31:13 -0300

    chore: setup para conectar ao MongoDb, instalação do dotenv para a string de conexão do mongo e instalação do mongoose

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDb conectado!");
+  })
+  .catch((err) => {
+    console.log("Erro ao tentar conectar no MongoDb!");
+  });
 
 const app = express();
 
