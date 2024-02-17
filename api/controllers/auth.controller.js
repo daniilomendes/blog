b1a743dfd009f4fa4887913f7d3dd31a2d64e3c9
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-17 17:33:18 -0300

    feat: add admin para usuários determinados

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -50,7 +50,7 @@ export const signin = async (req, res, next) => {
       return next(errorHandler(400, "Senha inválida."));
     }
 
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign({ id: validUser._id, isAdmin: validUser.isAdmin }, process.env.JWT_SECRET);
 
     const { password: pass, ...rest } = validUser._doc;
 
@@ -99,7 +99,7 @@ export const google = async (req, res, next) => {
 
       await newUser.save();
 
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      const token = jwt.sign({ id: user._id, isAdmin: newUser.isAdmin }, process.env.JWT_SECRET);
       const { password, ...rest } = user._doc;
 
       res
