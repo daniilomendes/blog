f69432306501b2ee87f2d836c70f6655078ed8ae
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-16 21:26:26 -0300

    feat: add e setup redux-persist para persistir os dados do usuário

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { store } from "./redux/store.js";
+import { store, persistor } from "./redux/store.js";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PersistGate>
 );
