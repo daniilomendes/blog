f7bdb8cf0a271eb30cb688b0154bdcd451081be2
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-20 20:03:33 -0300

    feat: add api route para criar posts

---
+++ api/controllers/post.controller.js
@@ -0,0 +1,33 @@
+import { errorHandler } from "../utils/error.js";
+import Post from "../models/post.model.js";
+
+export const create = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(
+      errorHandler(403, "Você não tem permissão para criar uma postagem.")
+    );
+  }
+
+  if (!req.body.title || !req.body.content) {
+    return next(errorHandler(400, "Forneça todos os campos obrigatórios."));
+  }
+
+  const slug = req.body.title
+    .split(" ")
+    .join("-")
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9-]/g, "");
+
+  const newPost = new Post({
+    ...req.body,
+    slug,
+    userId: req.user.id,
+  });
+
+  try {
+    const savedPost = await newPost.save();
+    res.status(201).json(savedPost);
+  } catch (error) {
+    next(error);
+  }
+};
