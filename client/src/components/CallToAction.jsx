0765d419defaf98b7a72c7d52ca607a4313269e4
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-22 19:29:06 -0300

    feat: add propaganda na página do post

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,31 @@
+import { Button } from "flowbite-react";
+import React from "react";
+
+export default function CallToAction() {
+  return (
+    <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
+      <div className="flex-1 justify-center flex flex-col">
+        <h2 className="text-2xl">Quer ver mais projetos como este?</h2>
+        <p className="text-gray-500 my-2">Confira esse e muitos outros no meu Portfólio:</p>
+        <Button
+          gradientDuoTone="purpleToPink"
+          className="rounded-tl-xl rounded-bl-none"
+        >
+          <a
+            href="https://portifolio-daniilomendes-projects.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Danilo Mendes Portfólio
+          </a>
+        </Button>
+      </div>
+      <div className="p-7 flex-1">
+        <img
+          src="https://wallpapercave.com/wp/wp8903914.jpg"
+          alt="imagem de propaganda do meu portfólio"
+        />
+      </div>
+    </div>
+  );
+}
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Button, Spinner } from "flowbite-react";
+import CallToAction from "../components/CallToAction";
 
 export default function PostPage() {
   const { postSlug } = useParams();
@@ -65,7 +66,10 @@ export default function PostPage() {
       </div>
 
       <div className="p-3 maxw2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{__html: post && post.content}}>
-        
+      </div>
+
+      <div className="max-w-4xl mx-auto w-full">
+        <CallToAction />
       </div>
     </main>
   );
