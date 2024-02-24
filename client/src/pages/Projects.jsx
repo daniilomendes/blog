57a57c13d81a12eb6e06122b767966bf59439821
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-24 16:36:49 -0300

    feat: add página de meus projetos

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import CallToAction from '../components/CallToAction'
 
 export default function Projects() {
   return (
-    <div>Projects</div>
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+      <h1 className='text-3xl font-semibold'>Projetos</h1>
+      <CallToAction />
+    </div>
   )
 }
