89ba437df8851b04becac34047a1a9660ab3a331
Danilo Mendes <danilo.mirandha@gmail.com>
2024-02-24 13:28:28 -0300

    style: modificação no tamanho do botão

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function CallToAction() {
   return (
     <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
-      <div className="flex-1 justify-center flex flex-col">
+      <div className="flex-1 justify-center flex flex-col items-center">
         <h2 className="text-2xl">Quer ver mais projetos como este?</h2>
         <p className="text-gray-500 my-2">Confira esse e muitos outros no meu Portfólio:</p>
         <Button
